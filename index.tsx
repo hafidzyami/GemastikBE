32d4c87fe5a313f89967d56c3f764d65eccd451a
hafidzyami <18221074@std.stei.itb.ac.id>
2024-09-22 22:40:32 +0700

    update expotoken

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import express from "express";
 import BodyParser from "body-parser";
-import * as FirebaseService from "./FirebaseService"
+import * as FirebaseService from "./FirebaseService";
 import Expo from "expo-server-sdk";
 
 const app = express();
@@ -9,22 +9,29 @@ const expo = new Expo();
 
 const jsonParser = BodyParser.json();
 
-app.post('/registerPushToken', jsonParser, async(req, res)=>{
-    const userId = String(req.body.userId);
-    const token = String(req.body.token);
-    await FirebaseService.saveToken(userId, token);
-    res.status(200).send("success");
-})
+app.post("/registerPushToken", jsonParser, async (req, res) => {
+  const userId = String(req.body.userId);
+  const token = String(req.body.token);
+  await FirebaseService.saveToken(userId, token);
+  res.status(200).send("success");
+});
 
-app.post(`/sample`, jsonParser, async(_, res) => {
-    expo.sendPushNotificationsAsync([
-        {
-            to : ["ExponentPushToken[tNKtAtJ1TG8P1KX0AIPc8z]", "ExponentPushToken[sq1UZBB8C5JYubZFRPRBFx]", "ExponentPushToken[7g-DWtOVu-QUq-exg_o3Ys]", "ExponentPushToken[KnuXdVG0kATOzhTnMJXK5-]"],
-            title : "Urgent Call from Paramedis",
-            body : "Terdapat pasien darurat pada ambulans!",
-        }
-    ]);
-    res.status(200).send("success")
-})
+app.post(`/sample`, jsonParser, async (_, res) => {
+  expo.sendPushNotificationsAsync([
+    {
+      to: [
+        "ExponentPushToken[tNKtAtJ1TG8P1KX0AIPc8z]",
+        "ExponentPushToken[sq1UZBB8C5JYubZFRPRBFx]",
+        "ExponentPushToken[7g-DWtOVu-QUq-exg_o3Ys]",
+        "ExponentPushToken[KnuXdVG0kATOzhTnMJXK5-]",
+        "ExponentPushToken[znMzwIO0_7DPmUW2rWGqs4]",
+        "ExponentPushToken[RKyFLoOAyT5lKRO4buEXON]"
+      ],
+      title: "Urgent Call from Paramedis",
+      body: "Terdapat pasien darurat pada ambulans!",
+    },
+  ]);
+  res.status(200).send("success");
+});
 
-app.listen(port, () => console.log(`running on port ${port}`))
+app.listen(port, () => console.log(`running on port ${port}`));
